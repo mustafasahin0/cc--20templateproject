a6b6afea2f1c3296bbed9029e2485f55d9780b92
mustafasahin0 <sahinmustafaenes4@gmail.com>
2026-01-11 14:38:43 -0700

    add error demonstration in new main.cpp file

---
+++ ERRORS_WARNINGS/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream> 
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+    int value = 7/0;
+    std::cout << "Value: " << value << std::endl;
+
+    return 0;
+}
--- FIRS_C_PROGRAM/main.cpp
+++ FIRS_C_PROGRAM/main.cpp
@@ -2,8 +2,9 @@
 
 
 int main () {
-
     std::cout << "Number1" << std::endl;
     std::cout << "Number2" << std::endl;
-    
+    std::cout << "Number3" << std::endl;
+
+    return 0;
 }
