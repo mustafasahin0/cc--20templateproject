399fafe5f22859a004a4ee0f61632937cc409cfa
mustafasahin0 <sahinmustafaenes4@gmail.com>
2026-01-11 19:57:28 -0700

    add main.cpp with number systems demonstration

---
+++ NUMBER_SYSTEMS/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main () {
+    int number1 = 15; // Decimal
+    int number2 = 017; // Octal
+    int number3 = 0x0F; // Hexadecimal
+    int number4 = 0b00001111; // Binary
+
+    std::cout << "Decimal: " << number1 << std::endl;
+    std::cout << "Octal: " << number2 << std::endl;
+    std::cout << "Hexadecimal: " << number3 << std::endl;
+    std::cout << "Binary: " << number4 << std::endl;
+    
+    return 0;
+}
